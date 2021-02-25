fdb49daaa8eecd0ba7e12369a2e67488133c128f
osmolenko <57684564+osmolenko@users.noreply.github.com>
2021-02-25 23:57:15 +0200

    fix: quotas

--- imports/ui/common/errorHandler.js
+++ imports/ui/common/errorHandler.js
@@ -1,13 +1,13 @@
 export function errorHandler(reason){
   switch (reason) {
-    case "Incorrect password":
-      return "Неверный пароль.";
-    case "User not found":
-      return "Пользователь не найден."
-    case "Username already exists.":
-      return "Пользователь с таким именем уже зарегистрирован."
+    case 'Incorrect password':
+      return 'Неверный пароль.';
+    case 'User not found':
+      return 'Пользователь не найден.'
+    case 'Username already exists.':
+      return 'Пользователь с таким именем уже зарегистрирован.'
     default:
       console.error(reason);
-      return "Неизвестная ошибка. Сообщите администратору."
+      return 'Неизвестная ошибка. Сообщите администратору.'
   }
 }
--- imports/ui/components/Tosses.jsx
+++ imports/ui/components/Tosses.jsx
@@ -17,9 +17,11 @@ export const Tosses = (props) => {
 	);
 
 	const changeTossesCountHandler = (type) => {
-
-		if ((tossesSkipCount - 20 < 0 && !type) || (tossesSkipCount + 20 > tossesCount && type)) {
-			console.error('No data.')
+		if (
+			(tossesSkipCount - 20 < 0 && !type) ||
+			(tossesSkipCount + 20 > tossesCount && type)
+		) {
+			console.error('No data.');
 		} else {
 			if (type) {
 				setTossesSkipCount(tossesSkipCount + 20);
@@ -31,45 +33,67 @@ export const Tosses = (props) => {
 
 	return (
 		<>
-		{tosses.length ? (
-			<table className='tosses-container'>
-				<thead>
-					<tr>
-						<th className='tosses-text toss-type'>
-							<b>Что выпало?</b>
-						</th>
-						<th className='tosses-text'>
-							<b>Когда?</b>
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					{tosses.map((toss, index) => (
-						<tr key={index}>
-							<td className='tosses-text toss-type'>
-								{toss.coin ? 'Орёл' : 'Решка'}
-							</td>
-							<td className='tosses-text'>
-								{toss.createdAt.toLocaleString('ru', {
-									day: 'numeric',
-									month: 'long',
-									year: 'numeric',
-									hour: 'numeric',
-									minute: 'numeric',
-									second: 'numeric',
-								})}
-							</td>
+			{tosses.length ? (
+				<table className='tosses-container'>
+					<thead>
+						<tr>
+							<th className='tosses-text toss-type'>
+								<b>Что выпало?</b>
+							</th>
+							<th className='tosses-text'>
+								<b>Когда?</b>
+							</th>
 						</tr>
-					))}
-				</tbody>
-			</table>
-		) : <p>Вы ещё ни разу не бросили монетку 🙁</p>}
+					</thead>
+					<tbody>
+						{tosses.map((toss, index) => (
+							<tr key={index}>
+								<td className='tosses-text toss-type'>
+									{toss.coin ? 'Орёл' : 'Решка'}
+								</td>
+								<td className='tosses-text'>
+									{toss.createdAt.toLocaleString('ru', {
+										day: 'numeric',
+										month: 'long',
+										year: 'numeric',
+										hour: 'numeric',
+										minute: 'numeric',
+										second: 'numeric',
+									})}
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			) : (
+				<p>Вы ещё ни разу не бросили монетку 🙁</p>
+			)}
 			{tossesCount > 20 ? (
-			<div className='pagination-container'>
-				<button className={`pagination-button ${tossesSkipCount - 20 < 0 ? 'pagination-inactive' : 'pagination-active'}`} onClick={() => changeTossesCountHandler(false)}>ᐊ</button>
-				<button className={`pagination-button ${Math.abs(tossesCount - tossesSkipCount) < 20  ? 'pagination-inactive' : 'pagination-active'}`} onClick={() => changeTossesCountHandler(true)}>ᐅ</button>
-			</div>
-			) : ""}
+				<div className='pagination-container'>
+					<button
+						className={`pagination-button ${
+							tossesSkipCount - 20 < 0
+								? 'pagination-inactive'
+								: 'pagination-active'
+						}`}
+						onClick={() => changeTossesCountHandler(false)}
+					>
+						ᐊ
+					</button>
+					<button
+						className={`pagination-button ${
+							Math.abs(tossesCount - tossesSkipCount) < 20
+								? 'pagination-inactive'
+								: 'pagination-active'
+						}`}
+						onClick={() => changeTossesCountHandler(true)}
+					>
+						ᐅ
+					</button>
+				</div>
+			) : (
+				''
+			)}
 		</>
 	);
 };
