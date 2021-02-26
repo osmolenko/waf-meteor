ad91b80ee5244f6069d10a21fce25df2e6082a3e
osmolenko <57684564+osmolenko@users.noreply.github.com>
2021-02-26 15:02:24 +0200

    feat: tests

--- tests/main.js
+++ tests/main.js
@@ -1,20 +1,68 @@
+import { CoinCollection } from '../imports/api/Coins';
+import { Meteor } from 'meteor/meteor'
 import assert from 'assert';
+import { resetDatabase } from 'meteor/xolvio:cleaner';
 
-describe('waf', function () {
-  it('package.json has correct name', async function () {
-    const { name } = await import('../package.json');
-    assert.strictEqual(name, 'waf');
+
+if (Meteor.isClient) {
+
+  describe('Test', function () {
+
+    resetDatabase()
+
+    it('You can register', function () {
+
+      Accounts.createUser(
+				{
+					username: 'test',
+					password: 'test',
+				},
+				function (err) {
+					if (err) throw err;
+				}
+			);
+
+      Meteor.logout();
+
+    })
+
+    it('You can login', function () {
+
+      Meteor.loginWithPassword('test', 'test', function (err) {
+				if (err) throw new Meteor.Error(err);
+			});
+
+      Meteor.logout();
+
+    })
+  
+    it('You can toss a coin with login', function () {
+
+      Meteor.loginWithPassword('test', 'test', function (err) {
+				if (err) console.error(err);
+			});
+  
+      Meteor.call('coin.toss', (err, result) => {
+        if (!err) {
+          assert.strictEqual(result, true | false) 
+        }
+      });
+  
+      Meteor.logout();
+  
+    });
+
+    it('You cannot toss a coin without login', function () {
+  
+      Meteor.call('coin.toss', (err) => {
+        if (err) assert.strictEqual(err.error, 'No auth.')
+      });  
+  
+    });
+
   });
+}
 
-  if (Meteor.isClient) {
-    it('client is not server', function () {
-      assert.strictEqual(Meteor.isServer, false);
-    });
-  }
 
-  if (Meteor.isServer) {
-    it('server is not client', function () {
-      assert.strictEqual(Meteor.isClient, false);
-    });
-  }
-});
+
+
